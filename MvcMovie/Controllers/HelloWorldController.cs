e4e5c596c4827ba8f9e3862b321a76089b0b12b2
Simran Priya <simranmadhupriya@gmail.com>
2021-08-18 12:35:13 +0530

    ASP.NET Core Web App Implemented

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/ 
+
+        /*    public string Index()
+            {
+                return "This is my <b>default</b> action...";
+            }*/
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome1(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public string WelcomeAgain(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+
+        public string WelcomeAnotherTime(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
